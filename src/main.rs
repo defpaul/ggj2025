975d7be930d8289d8150e276399d89b3d98a259c
paul <paul_Obercheker@gmx.de>
2025-01-25 16:36:09 +0100

    half funktenig system

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,29 @@ mod story;
 
 use bevy::prelude::*;
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
-
+use crate::story::Situation;
 
 const NORMAL_BUTTON: Color = Color::srgb(0.15, 0.15, 0.15);
 const HOVERED_BUTTON: Color = Color::srgb(0.25, 0.25, 0.25);
 const PRESSED_BUTTON: Color = Color::srgb(0.35, 0.75, 0.35);
 
+#[derive(Resource)]
+struct NextSituation{
+   next: bool,
+    next_id: String
+}
+
 fn main() {
       App::new()
+          .insert_resource(NextSituation{next:true, next_id: "shop_greating".to_string()})
+          .insert_resource(Situation{..default()})
           .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
           .add_plugins(WorldInspectorPlugin::new())
           .add_systems(Startup, setup)
           .add_systems(Startup, button::spawn)
           .add_systems(Startup, buble::spawn)
           .add_systems(Update, button::status_update)
+          .add_systems(Update, story::next)
 
           .run();
 }
--- src/story.rs
+++ src/story.rs
@@ -2,39 +2,54 @@ use bevy::prelude::*;
 use std::fs;
 use serde::{Deserialize, Serialize};
 use serde_json::Result;
+use crate::NextSituation;
 
 #[derive(Serialize, Deserialize)]
-struct Dialog{
-    speker: String,
-    text: String
+pub struct Dialog{
+    pub talker: String,
+    pub text: String
 }
 
 
-#[derive(Serialize, Deserialize)]
-struct Anser{
-    short: String,
-    long: String,
-    next: String
+#[derive(Serialize, Deserialize, Default)]
+pub struct Anser{
+    pub short: String,
+    pub long: String,
+    pub next: String
 }
 
-#[derive(Serialize, Deserialize)]
-struct Situation{
-    speker: String,
-    dialog: Vec<String>,
-    dialog_stage: u16,
-    anser: [Anser; 3]
+#[derive(Serialize, Deserialize, Resource, Default)]
+pub struct Situation{
+    pub person: String,
+    pub dialog: Vec<Dialog>,
+    pub ansers: [Anser; 3]
 }
 
 fn pars_situation(
     next_situation: &str,
-){
+) -> Situation {
 
-    let file = fs::read_to_string("story.json")
+    let path = format!("{next_situation}.json");
+    println!("{}", path);
+
+    let file = fs::read_to_string(path)
         .expect("culd not open");
 
-    let json: serde_json::Value = serde_json::from_str(&file)
+    let situation: Situation = serde_json::from_str(&file)
         .expect("some err");
+    situation
 
-    let situation = json.get(next_situation)
-        .expect("some other err");
+}
+
+pub fn next(
+    mut next_situation: ResMut<NextSituation>,
+    mut situation: ResMut<Situation>,
+){
+    if next_situation.next {
+
+        *situation= pars_situation(&next_situation.next_id);
+
+        next_situation.next = false
+
+    }
 }
